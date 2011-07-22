fa40b582b3ea1d0f534c3641a4f516b4c8f2b22d
Lucas Jenß <lucas@x3ro.de>
2011-07-22 17:03:50 +0200

    Fix default deployment stage to match our server structure

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,11 +3,11 @@ server '<remote server, e.g. go11.gosign.de>', :app, :web, :primary => true
 # Path we want to deploy to. Note that this is not
 # htdocs/, but the folder we cloned our "DefaultProjektStruktur"-
 # repository to!
-set :deploy_to, "/home/kunden/webs/#{application}"
+set :deploy_to, "/home/kunden/webs/#{customer}/#{application}/#{stage}"
 
 # User and group of the deploy user on the remote server
-set :user, "gosign"
-set :group, "gosign"
+set :user, "<user>"
+set :group, "<group>"
 
 
 # Which branch of our repository do we want to check out?
@@ -17,9 +17,10 @@ set :branch, "master"
 # Set the correct group and access rights for all our files.
 # This may be different for every server, and therefore is
 # is located in the stage file.
-namespace :deploy do
-    task :permissions do
-        run "cd #{deploy_to}/current && chgrp -Rf www-data . && chmod -Rf 775 ."
-    end
-end
-after :deploy, "deploy:permissions"
+# TODO: Implement properly
+#namespace :deploy do
+#    task :permissions do
+#        run "cd #{deploy_to}/current && chgrp -Rf www-data . && chmod -Rf 775 ."
+#    end
+#end
+#after :deploy, "deploy:permissions"
