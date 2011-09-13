21a05bbe602eff22bc7e876ae0d2500133f8af86
Caspar Stuebs <caspar@gosign.de>
2011-09-13 14:44:44 +0200

    go_printbutton cleanup

--- typo3conf/ext/go_printbutton/ext_emconf.php
+++ typo3conf/ext/go_printbutton/ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "goprintbutton"
+# Extension Manager/Repository config file for ext: "go_printbutton"
 #
 # Auto generated 03-04-2008 17:13
 #
@@ -11,13 +11,13 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'goPrintButton',
+	'title' => 'Gosign Print Button',
 	'description' => 'Druckansicht und Buttons',
 	'category' => 'fe',
 	'author' => 'Marius Stuebs',
 	'author_email' => 'marius@gosign.de',
 	'shy' => '',
-	'dependencies' => 'tx_pdfgenerator, tv_pdfgen',
+	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -31,15 +31,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'version' => '1.0.0',
 	'constraints' => array(
-		'depends' => array("tx_pdfgenerator", "tv_pdfgen"
+		'depends' => array(
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"82bc";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1dd5";s:14:"ext_tables.php";s:4:"b718";s:16:"print_button.png";s:4:"58f3";s:19:"doc/wizard_form.dat";s:4:"4c21";s:20:"doc/wizard_form.html";s:4:"15c4";s:39:"static/_ext_goPrintButton/constants.txt";s:4:"d41d";s:35:"static/_ext_goPrintButton/setup.txt";s:4:"19a3";}',
-	'suggests' => array("fileadmin/templates/images/pdf_button.png", "fileadmin/templates/images/print_button.png"
+	'_md5_values_when_last_written' => 'a:4:{s:12:"ext_icon.gif";s:4:"1dd5";s:14:"ext_tables.php";s:4:"c8ba";s:24:"ext_typoscript_setup.txt";s:4:"0724";s:20:"res/print_button.png";s:4:"58f3";}',
+	'suggests' => array(
 	),
 );
 
--- typo3conf/ext/go_printbutton/ext_tables.php
+++ typo3conf/ext/go_printbutton/ext_tables.php
@@ -1,5 +1,4 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-## WOP:[ts][1]
-##t3lib_extMgm::addStaticFile($_EXTKEY,'static/_ext_goPrintButton/', '+ext goPrintButton');
+
 ?>
