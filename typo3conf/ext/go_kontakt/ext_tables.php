1c9d59d1331bb8d5247be4afcaaf3c84783bf417
Caspar Stuebs <caspar@gosign.de>
2011-09-14 17:41:01 +0200

    go_kontakt: add new DB/TCA fields and labels

--- typo3conf/ext/go_kontakt/ext_tables.php
+++ typo3conf/ext/go_kontakt/ext_tables.php
@@ -3,6 +3,9 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+$LANG = t3lib_div::makeInstance('language');
+$LANG->init($BE_USER->uc['lang']);
+
 $tempColumns = array (
     'tx_gokontakt_redirect' => array (        
         'exclude' => 0,        
@@ -25,38 +28,108 @@ $tempColumns = array (
             )
         )
     ),
-	'tx_gokontakt_emailFrom' => array (        
-        'exclude' => 0,        
-        'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailFrom',        
-        'config' => array (
-            'type' => 'input',    
-            'size' => '30',    
-            'eval' => 'required',
-        )
-    ),
-	'tx_gokontakt_emailFromName' => array (        
-        'exclude' => 0,        
-        'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailFromName',        
-        'config' => array (
-            'type' => 'input',    
-            'size' => '30',    
-        )
-    ),
-	'tx_gokontakt_emailToAdmin' => array (        
-        'exclude' => 0,        
-        'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailToAdmin',        
-        'config' => array (
-            'type' => 'input',    
-            'size' => '30',    
-            'eval' => 'required',
-        )
-    ),
+	/* EMAIL FIELDS */
+	'tx_gokontakt_emailFrom' => array (
+		'exclude' => 0,
+		'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailFrom',
+		'config' => array (
+			'type' => 'input',
+			'size' => '50',
+			'eval' => 'required',
+		)
+	),
+	'tx_gokontakt_emailFromName' => array (
+		'exclude' => 0,
+		'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailFromName',
+		'config' => array (
+			'type' => 'input',
+			'size' => '50',
+			'eval' => 'required',
+		)
+	),
+	'tx_gokontakt_emailToAdmin' => array (
+		'exclude' => 0,
+		'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailToAdmin',
+		'config' => array (
+			'type' => 'input',
+			'size' => '50',
+			'eval' => 'required',
+		)
+	),
+	'tx_gokontakt_emailSubject' => array (
+		'exclude' => 0,
+		'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailSubject',
+		'config' => array (
+			'type' => 'input',
+			'size' => '50',
+			'eval' => 'required',
+			'default' => $LANG->sL('LLL:EXT:go_kontakt/locallang_db.xml:default.tx_gokontakt_emailSubject')
+		),
+	),
+	'tx_gokontakt_emailBody' => array (
+		'exclude' => 0,
+		'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailBody',
+		'config' => array (
+			'type' => 'text',
+			'cols' => '80',
+			'rows' => '10',
+			'eval' => 'required',
+			'default' => $LANG->sL('LLL:EXT:go_kontakt/locallang_db.xml:default.tx_gokontakt_emailBody')
+		),
+	),
+	'tx_gokontakt_emailAdminSubject' => array (
+		'exclude' => 0,
+		'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailAdminSubject',
+		'config' => array (
+			'type' => 'input',
+			'size' => '50',
+			'eval' => 'required',
+			'default' => $LANG->sL('LLL:EXT:go_kontakt/locallang_db.xml:default.tx_gokontakt_emailAdminSubject')
+		)
+	),
+	'tx_gokontakt_emailAdminBody' => array (
+		'exclude' => 0,
+		'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailAdminBody',
+		'config' => array (
+			'type' => 'text',
+			'cols' => '80',
+			'rows' => '10',
+			'eval' => 'required',
+			'default' => $LANG->sL('LLL:EXT:go_kontakt/locallang_db.xml:default.tx_gokontakt_emailAdminBody')
+		)
+	),
+	'tx_gokontakt_emailNewsletterSubject' => array (
+		'exclude' => 0,
+		'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailNewsletterSubject',
+		'config' => array (
+			'type' => 'input',
+			'size' => '50',
+			'eval' => 'required',
+			'default' => $LANG->sL('LLL:EXT:go_kontakt/locallang_db.xml:default.tx_gokontakt_emailNewsletterSubject')
+		)
+	),
+	'tx_gokontakt_emailNewsletterBody' => array (
+		'exclude' => 0,
+		'label' => 'LLL:EXT:go_kontakt/locallang_db.xml:tt_content.tx_gokontakt_emailNewsletterBody',
+		'config' => array (
+			'type' => 'text',
+			'cols' => '80',
+			'rows' => '10',
+			'eval' => 'required',
+			'default' => $LANG->sL('LLL:EXT:go_kontakt/locallang_db.xml:default.tx_gokontakt_emailNewsletterBody')
+		)
+	)
 );
 
 t3lib_div::loadTCA('tt_content');
 t3lib_extMgm::addTCAcolumns('tt_content',$tempColumns,1);
 
-$TCA['tt_content']['types'][$_EXTKEY . '_piKontakt']['showitem'] = 'CType;;4;button;1-1-1, header;;3;;2-2-2,pages,tx_gokontakt_emailFrom,tx_gokontakt_emailFromName,tx_gokontakt_emailToAdmin';
+$TCA['tt_content']['types'][$_EXTKEY . '_piKontakt']['showitem'] = '
+--div--;LLL:EXT:go_kontakt/locallang_db.xml:div.data, CType;;4;button;1-1-1, header;;3;;2-2-2,pages,
+--div--;LLL:EXT:go_kontakt/locallang_db.xml:div.email, tx_gokontakt_emailFrom,tx_gokontakt_emailFromName,tx_gokontakt_emailToAdmin,
+--div--;LLL:EXT:go_kontakt/locallang_db.xml:div.emailtext, tx_gokontakt_emailSubject,tx_gokontakt_emailBody,tx_gokontakt_emailAdminSubject,tx_gokontakt_emailAdminBody,
+--div--;LLL:EXT:go_kontakt/locallang_db.xml:div.newsletter, tx_gokontakt_emailNewsletterSubject,tx_gokontakt_emailNewsletterBody,
+';
 
 // Contact Form
 
--- typo3conf/ext/go_kontakt/ext_tables.sql
+++ typo3conf/ext/go_kontakt/ext_tables.sql
@@ -5,5 +5,11 @@ CREATE TABLE tt_content (
     tx_gokontakt_redirect tinytext,
 	tx_gokontakt_emailFrom tinytext,
 	tx_gokontakt_emailFromName tinytext,
+	tx_gokontakt_emailSubject tinytext,
+	tx_gokontakt_emailBody text,
 	tx_gokontakt_emailToAdmin tinytext,
+	tx_gokontakt_emailAdminSubject tinytext,
+	tx_gokontakt_emailAdminBody text,
+	tx_gokontakt_emailNewsletterSubject tinytext,
+	tx_gokontakt_emailNewsletterBody text
 );
