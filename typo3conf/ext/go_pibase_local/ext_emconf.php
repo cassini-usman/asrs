eea37fb03071eca51f619a5595757f64edc9cda3
Caspar Stuebs <caspar@gosign.de>
2011-09-22 13:21:54 +0200

    add extension go_pibase_local

---
+++ typo3conf/ext/go_pibase_local/ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "go_pibase_local"
+#
+# Auto generated 21-04-2009 18:09
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Gosign Pluginbase - Local',
+	'description' => '',
+	'category' => 'fe',
+	'author' => 'Lucas Jenß',
+	'author_email' => 'lucas@gosign.de',
+	'shy' => '',
+	'dependencies' => 'dam',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'Gosign media. GmbH',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'go_pibase' => '1.2.2-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:16:"ext_autoload.php";s:4:"85f9";s:12:"ext_icon.gif";s:4:"461a";s:17:"ext_localconf.php";s:4:"c8ba";s:14:"ext_tables.php";s:4:"a0fd";s:30:"lib/class.tx_gopibaselocal.php";s:4:"3589";}',
+);
+
+?>
