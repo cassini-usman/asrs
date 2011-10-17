ff6b909d70dd944a6c8a099f8eef9f1b7689b11c
Caspar Stuebs <caspar@gosign.de>
2011-10-17 16:13:58 +0200

    go_tsconfig: update browser conditions, add body tag class

--- typo3conf/ext/go_tsconfig/ext_ts_basic.ts
+++ typo3conf/ext/go_tsconfig/ext_ts_basic.ts
@@ -27,12 +27,54 @@ page.includeCSS.content = fileadmin/templates/css/content.css
 page.includeCSS.contentteiler = fileadmin/templates/css/contentteiler.css
 page.includeCSS.footer_menu = fileadmin/templates/css/footer_menu.css
 
-[useragent = *Mozilla*] && [system = mac]
+[useragent = *iPad*]
+	page.includeCSS.iPad = fileadmin/templates/css/iPad.css
+	page.bodyTagAdd = class="ipad"
+[global]
+
+[browser = msie] && [system = win] && [version = 7]
+	page.includeCSS.ie7 = fileadmin/templates/css/ie7.css
+	page.bodyTagAdd = class="ie7"
+[global]
+
+[browser = msie] && [system = win] && [version = 8]
+	page.includeCSS.ie8 = fileadmin/templates/css/ie8.css
+	page.bodyTagAdd = class="ie8"
+[global]
+
+[browser = msie] && [system = win] && [version = 9]
+	page.includeCSS.ie9 = fileadmin/templates/css/ie9.css
+	page.bodyTagAdd = class="ie9"
+[global]
+
+[browser = chrome] && [system = win]
+	page.includeCSS.chrome_win = fileadmin/templates/css/chrome_win.css
+	page.bodyTagAdd = class="chrome_win"
+[global]
+
+[browser = chrome] && [system = mac]
+	page.includeCSS.chrome_mac = fileadmin/templates/css/chrome_mac.css
+	page.bodyTagAdd = class="chrome_mac"
+[global]
+
+[useragent = *Firefox*] && [system = win]
+	page.includeCSS.firefox_win = fileadmin/templates/css/firefox_win.css
+	page.bodyTagAdd = class="firefox_win"
+[global]
+
+[useragent = *Firefox*] && [system = mac]
 	page.includeCSS.firefox_mac = fileadmin/templates/css/firefox_mac.css
+	page.bodyTagAdd = class="firefox_mac"
+[global]
+
+[useragent = *Safari*] && [system = win]
+	page.includeCSS.safari_win = fileadmin/templates/css/safari_win.css
+	page.bodyTagAdd = class="safari_win"
 [global]
 
 [useragent = *Safari*] && [system = mac]
 	page.includeCSS.safari_mac = fileadmin/templates/css/safari_mac.css
+	page.bodyTagAdd = class="safari_mac"
 [global]
 
 # includeJS libs fileadmin/templates/javascript/..
