f2d04b1560537e2af73e23c96c3bff74fd94ca23
Caspar Stuebs <caspar@gosign.de>
2011-10-25 13:45:51 +0200

    go_tsconfig update ext_emconf.php, raise version to 1.1.1

--- typo3conf/ext/go_tsconfig/ext_emconf.php
+++ typo3conf/ext/go_tsconfig/ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Gosign media. GmbH',
-	'version' => '1.1.0',
+	'version' => '1.1.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:21:"ext_conf_template.txt";s:4:"dbe4";s:12:"ext_icon.gif";s:4:"70c2";s:17:"ext_localconf.php";s:4:"8d49";s:14:"ext_tables.php";s:4:"474c";s:14:"ext_tables.sql";s:4:"83b8";s:20:"ext_ts_afterlocal.ts";s:4:"4819";s:15:"ext_ts_basic.ts";s:4:"6ceb";s:23:"ext_ts_browser_error.ts";s:4:"7652";s:16:"ext_ts_header.ts";s:4:"c1f4";s:15:"ext_ts_local.ts";s:4:"68e1";s:20:"ext_ts_naviFooter.ts";s:4:"3cc5";s:18:"ext_ts_naviLeft.ts";s:4:"fcf0";s:17:"ext_ts_naviTop.ts";s:4:"1120";s:19:"ext_ts_printMenu.ts";s:4:"f33d";s:24:"ext_typoscript_setup.txt";s:4:"591a";s:13:"locallang.xml";s:4:"f804";s:16:"locallang_db.xml";s:4:"7956";s:19:"page_ts_rootPage.ts";s:4:"0295";s:21:"res/browser_error.png";s:4:"7c71";}',
+	'_md5_values_when_last_written' => 'a:19:{s:21:"ext_conf_template.txt";s:4:"dbe4";s:12:"ext_icon.gif";s:4:"70c2";s:17:"ext_localconf.php";s:4:"8d49";s:14:"ext_tables.php";s:4:"474c";s:14:"ext_tables.sql";s:4:"83b8";s:20:"ext_ts_afterlocal.ts";s:4:"4819";s:15:"ext_ts_basic.ts";s:4:"a216";s:23:"ext_ts_browser_error.ts";s:4:"7652";s:16:"ext_ts_header.ts";s:4:"c1f4";s:15:"ext_ts_local.ts";s:4:"68e1";s:20:"ext_ts_naviFooter.ts";s:4:"3cc5";s:18:"ext_ts_naviLeft.ts";s:4:"fcf0";s:17:"ext_ts_naviTop.ts";s:4:"1120";s:19:"ext_ts_printMenu.ts";s:4:"f33d";s:24:"ext_typoscript_setup.txt";s:4:"591a";s:13:"locallang.xml";s:4:"f804";s:16:"locallang_db.xml";s:4:"7956";s:19:"page_ts_rootPage.ts";s:4:"0295";s:21:"res/browser_error.png";s:4:"7c71";}',
 );
 
 ?>
