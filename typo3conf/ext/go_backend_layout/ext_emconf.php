53d6dae5d01275fbcf924f3aed36ab72f5097082
Caspar Stuebs <caspar@gosign.de>
2011-12-21 13:51:22 +0100

    go_backend_layout raise version to 1.8.3

--- typo3conf/ext/go_backend_layout/ext_emconf.php
+++ typo3conf/ext/go_backend_layout/ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "go_backend_layout"
+# Extension Manager/Repository config file for ext "go_backend_layout".
 #
-# Auto generated 08-09-2008 11:01
+# Auto generated 21-12-2011 13:49
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'mansoor@gosign.de',
 	'shy' => '',
 	'dependencies' => 'templavoila',
-	'conflicts' => '',
+	'conflicts' => 't3skin_improved,me_templavoilalayout,me_templavoilalayout2,modern_skin,tm_tvpagemodule',
 	'priority' => '',
 	'module' => '',
 	'state' => 'beta',
@@ -29,23 +29,25 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Gosign media. GmbH',
-	'version' => '1.8.2',
+	'version' => '1.8.3',
 	'constraints' => array(
 		'depends' => array(
-			'templavoila'			=>	'templavoila'
+			'templavoila' => 'templavoila',
 		),
 		'conflicts' => array(
-			't3skin_improved' 		=> 	't3skin_improved',
-			'me_templavoilalayout'	=>	'me_templavoilalayout',
-			'me_templavoilalayout2'	=>	'me_templavoilalayout2',
-			'modern_skin'			=> 	'modern_skin',
-			'tm_tvpagemodule' 		=> 	'tm_tvpagemodule'
+			't3skin_improved' => 't3skin_improved',
+			'me_templavoilalayout' => 'me_templavoilalayout',
+			'me_templavoilalayout2' => 'me_templavoilalayout2',
+			'modern_skin' => 'modern_skin',
+			'tm_tvpagemodule' => 'tm_tvpagemodule',
 		),
 		'suggests' => array(
-			'dam'					=>	'dam'
+			'dam' => 'dam',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:41:{s:9:"ChangeLog";s:4:"df09";s:12:"ext_icon.gif";s:4:"461a";s:17:"ext_localconf.php";s:4:"34bf";s:14:"ext_tables.php";s:4:"539a";s:14:"ext_tables.sql";s:4:"d592";s:21:"go_backend_layout.css";s:4:"836f";s:13:"locallang.xml";s:4:"815c";s:27:"images/pageview_bullets.png";s:4:"c596";s:26:"images/pageview_header.png";s:4:"391c";s:24:"images/pageview_html.png";s:4:"c37e";s:25:"images/pageview_image.png";s:4:"3913";s:25:"images/pageview_table.png";s:4:"7855";s:24:"images/pageview_text.png";s:4:"c07b";s:27:"images/pageview_textpic.png";s:4:"2868";s:28:"images/pageview_textpic2.png";s:4:"f350";s:25:"images/wizard_bullets.png";s:4:"7b6f";s:24:"images/wizard_header.png";s:4:"5429";s:22:"images/wizard_html.png";s:4:"60f3";s:24:"images/wizard_image2.png";s:4:"b835";s:23:"images/wizard_table.png";s:4:"fc45";s:22:"images/wizard_text.png";s:4:"a0ca";s:26:"images/wizard_textpic2.png";s:4:"51f2";s:18:"lib/draganddrop.js";s:4:"a9b7";s:13:"lib/jQuery.js";s:4:"3eb4";s:25:"lib/jQuery.stickynotes.js";s:4:"e6ac";s:15:"lib/jQueryUi.js";s:4:"2701";s:17:"lib/wizardList.js";s:4:"b01b";s:22:"moduls/config/conf.php";s:4:"61f5";s:23:"moduls/config/index.php";s:4:"ae37";s:28:"moduls/config/moduleicon.gif";s:4:"461a";s:47:"moduls/db_list_extra/class.ux_db_list_extra.php";s:4:"30a1";s:37:"moduls/db_list_extra/db_list_hook.php";s:4:"d278";s:59:"moduls/templavoila/class.ux_tx_templavoila_mod1_wizards.php";s:4:"c2db";s:54:"moduls/templavoila/class.ux_tx_templavoila_module1.php";s:4:"82e4";s:32:"moduls/templavoila/locallang.xml";s:4:"7f16";s:43:"moduls/templavoila/ux_db_new_content_el.php";s:4:"76dc";s:31:"moduls/tt_news/hook.tt_news.php";s:4:"d570";s:34:"res/doktype_icon_pagecontainer.png";s:4:"1028";s:18:"res/notesClose.png";s:4:"e3b2";s:17:"res/notesIcon.png";s:4:"39eb";s:19:"res/notesResize.png";s:4:"1b24";}',
+	'_md5_values_when_last_written' => 'a:41:{s:9:"ChangeLog";s:4:"df09";s:12:"ext_icon.gif";s:4:"461a";s:17:"ext_localconf.php";s:4:"34bf";s:14:"ext_tables.php";s:4:"539a";s:14:"ext_tables.sql";s:4:"d592";s:21:"go_backend_layout.css";s:4:"836f";s:13:"locallang.xml";s:4:"815c";s:27:"images/pageview_bullets.png";s:4:"c596";s:26:"images/pageview_header.png";s:4:"391c";s:24:"images/pageview_html.png";s:4:"c37e";s:25:"images/pageview_image.png";s:4:"3913";s:25:"images/pageview_table.png";s:4:"7855";s:24:"images/pageview_text.png";s:4:"c07b";s:27:"images/pageview_textpic.png";s:4:"2868";s:28:"images/pageview_textpic2.png";s:4:"f350";s:25:"images/wizard_bullets.png";s:4:"7b6f";s:24:"images/wizard_header.png";s:4:"5429";s:22:"images/wizard_html.png";s:4:"60f3";s:24:"images/wizard_image2.png";s:4:"b835";s:23:"images/wizard_table.png";s:4:"fc45";s:22:"images/wizard_text.png";s:4:"a0ca";s:26:"images/wizard_textpic2.png";s:4:"51f2";s:18:"lib/draganddrop.js";s:4:"a9b7";s:13:"lib/jQuery.js";s:4:"3eb4";s:25:"lib/jQuery.stickynotes.js";s:4:"e6ac";s:15:"lib/jQueryUi.js";s:4:"2701";s:17:"lib/wizardList.js";s:4:"b01b";s:22:"moduls/config/conf.php";s:4:"61f5";s:23:"moduls/config/index.php";s:4:"ae37";s:28:"moduls/config/moduleicon.gif";s:4:"461a";s:47:"moduls/db_list_extra/class.ux_db_list_extra.php";s:4:"30a1";s:37:"moduls/db_list_extra/db_list_hook.php";s:4:"d278";s:59:"moduls/templavoila/class.ux_tx_templavoila_mod1_wizards.php";s:4:"c2db";s:54:"moduls/templavoila/class.ux_tx_templavoila_module1.php";s:4:"8445";s:32:"moduls/templavoila/locallang.xml";s:4:"7f16";s:43:"moduls/templavoila/ux_db_new_content_el.php";s:4:"76dc";s:31:"moduls/tt_news/hook.tt_news.php";s:4:"d570";s:34:"res/doktype_icon_pagecontainer.png";s:4:"1028";s:18:"res/notesClose.png";s:4:"e3b2";s:17:"res/notesIcon.png";s:4:"39eb";s:19:"res/notesResize.png";s:4:"1b24";}',
+	'suggests' => array(
+	),
 );
 
 ?>
