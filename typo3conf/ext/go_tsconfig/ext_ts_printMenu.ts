358891d230b57085bb3da385dc999a2d513662fe
Caspar Stuebs <caspar@gosign.de>
2012-02-09 16:40:39 +0100

    go_tsconfig add print configurations

--- typo3conf/ext/go_tsconfig/ext_ts_printMenu.ts
+++ typo3conf/ext/go_tsconfig/ext_ts_printMenu.ts
@@ -1,2 +1,12 @@
+page.includeCSS {
+	printCss = fileadmin/templates/css/print.css
+	printCss.media = print
+}
+
+[globalVar = GP:print = 1]
+	page.includeCSS.printView = fileadmin/templates/css/print.css
+	page.includeJS.printView = fileadmin/templates/javascript/print.js
+[global]
+
 printmenu = TEXT
 printmenu.value = printmenu
