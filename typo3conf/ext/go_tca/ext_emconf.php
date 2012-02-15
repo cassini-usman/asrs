f58b9fbf3987b5b8ac22872a696f791375a0c3f0
Caspar Stuebs <caspar@gosign.de>
2012-02-15 11:33:54 +0100

    go_tca update ext_tables.sql add 'NOT NULL' to field definitions - raise version to 1.0.2

--- typo3conf/ext/go_tca/ext_emconf.php
+++ typo3conf/ext/go_tca/ext_emconf.php
@@ -1,46 +1,46 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "go_tca"
+# Extension Manager/Repository config file for ext "go_tca".
 #
-# Auto generated 24-04-2009 04:15
+# Auto generated 15-02-2012 11:30
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Go TCA',
-	'description' => '',
+	'description' => 'This extension defines some default fields for table tt_content',
 	'category' => 'fe',
-	'author' => 'Gosign media.',
+	'author' => '',
 	'author_email' => 'michael@gosign.de',
 	'shy' => '',
-	'dependencies' => 'cms',
+	'dependencies' => 'cms,go_pibase',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author_company' => '',
-	'version' => '1.0.1',
+	'author_company' => 'Gosign media. GmbH',
+	'version' => '1.0.2',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
-			'go_pibase' => ''
+			'go_pibase' => '',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:9:"README.md";s:4:"f235";s:12:"ext_icon.gif";s:4:"461a";s:17:"ext_localconf.php";s:4:"3b5c";s:14:"ext_tables.php";s:4:"6c39";s:14:"ext_tables.sql";s:4:"66aa";s:24:"ext_typoscript_setup.txt";s:4:"4464";s:16:"locallang_db.xml";s:4:"6156";}',
+	'_md5_values_when_last_written' => 'a:7:{s:9:"README.md";s:4:"f235";s:12:"ext_icon.gif";s:4:"461a";s:17:"ext_localconf.php";s:4:"3b5c";s:14:"ext_tables.php";s:4:"6c39";s:14:"ext_tables.sql";s:4:"95c6";s:24:"ext_typoscript_setup.txt";s:4:"4464";s:16:"locallang_db.xml";s:4:"6156";}',
 	'suggests' => array(
 	),
 );
