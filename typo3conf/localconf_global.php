421545a90ee7914b479b78297a0e55d4bec965ac
Caspar Stuebs <caspar@gosign.de>
2012-05-11 00:31:17 +0200

    localconf_global.php - new installtool password

--- typo3conf/localconf_global.php
+++ typo3conf/localconf_global.php
@@ -30,7 +30,7 @@ $TYPO3_CONF_VARS['BE']['explicitADmode'] = 'explicitAllow';
 $TYPO3_CONF_VARS['BE']['fileCreateMask'] = '0664';
 $TYPO3_CONF_VARS['BE']['folderCreateMask'] = '0775';
 $TYPO3_CONF_VARS['BE']['disable_exec_function'] = '0';
-$TYPO3_CONF_VARS['BE']['installToolPassword'] = '0bd0b0e0c5754271d8dc5ea254288c82';
+$TYPO3_CONF_VARS['BE']['installToolPassword'] = '2a01663b2cc1f1dc9d75b9df949c05db';
 $TYPO3_CONF_VARS['BE']['versionNumberInFilename'] = '0';
 
 $TYPO3_CONF_VARS['FE']['disableNoCacheParameter'] = '0';
